bcf21e2942c15df0f70b415bb906f4816825c2e3
Samuel <30960735+slownie@users.noreply.github.com>
2026-06-29 22:38:09 -0400

    i should come up with better names for this

--- src/overworld/overworldMenu/baseMenu/OverworldBaseMenu.cs
+++ src/overworld/overworldMenu/baseMenu/OverworldBaseMenu.cs
@@ -3,26 +3,103 @@ using System;
 
 public partial class OverworldBaseMenu : UIOverworldMenuBase
 {
+	[Export] PackedScene menuEntryScene;
 	[Export] PackedScene partyMenuEntryScene;
 	
+	
 	Godot.Collections.Array<UIPartyMenuEntry> _partyEntries = [];
+
+	VBoxContainer _menuContainer;
+	VBoxContainer _partyContainer;
 	UIGoldDisplay _goldDisplay;
+
+	Label _currentOption;
+
+	Godot.Collections.Array<UIBaseMenuEntry> _menuEntries = [];
+	UIBaseMenuEntry _currentEntry;
+	UIBaseMenuEntry currentEntry
+	{
+		get { return _currentEntry; }
+		set
+		{
+			if (_currentEntry != null) _currentEntry.Highlighted = false;
+
+			_currentEntry = value;
+			_currentEntry.Highlighted = true;
+		}
+	}
+
+	int _index;
+	int index
+	{
+		get { return _index; }
+		set
+		{
+			_index = value;
+			if (_menuEntries.Count <= _index) _index = 0;
+			if (_index < 0) _index = _menuEntries.Count - 1;
+			currentEntry = _menuEntries[_index];
+		}
+	}
 
     public override void _Ready()
 	{
+		_menuContainer = GetNode<VBoxContainer>("MenuEntries");
+		_partyContainer = GetNode<VBoxContainer>("PartyEntries");
+
 		_goldDisplay = GetNode<UIGoldDisplay>("UIGoldDisplay");
+	}
+
+    public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("MoveUp")) index -= 1;
+		if (@event.IsActionPressed("MoveDown")) index += 1;
 	}
 
 	public void Setup(GameState gameState)
 	{
+		// Menu Setup
+		UIBaseMenuEntry skillsEntry = menuEntryScene.Instantiate() as UIBaseMenuEntry;
+		_menuContainer.AddChild(skillsEntry);
+		skillsEntry.Setup(UIBaseMenuEntry.MainMenuType.Skills);
+		_menuEntries.Add(skillsEntry);
+
+		UIBaseMenuEntry itemsEntry = menuEntryScene.Instantiate() as UIBaseMenuEntry;
+		_menuContainer.AddChild(itemsEntry);
+		itemsEntry.Setup(UIBaseMenuEntry.MainMenuType.Items);
+		_menuEntries.Add(itemsEntry);
+
+		UIBaseMenuEntry equipmentEntry = menuEntryScene.Instantiate() as UIBaseMenuEntry;
+		_menuContainer.AddChild(equipmentEntry);
+		equipmentEntry.Setup(UIBaseMenuEntry.MainMenuType.Equipment);
+		_menuEntries.Add(equipmentEntry);
+
+		UIBaseMenuEntry partyEntry = menuEntryScene.Instantiate() as UIBaseMenuEntry;
+		_menuContainer.AddChild(partyEntry);
+		partyEntry.Setup(UIBaseMenuEntry.MainMenuType.Party);
+		_menuEntries.Add(partyEntry);
+
+		UIBaseMenuEntry settingsEntry = menuEntryScene.Instantiate() as UIBaseMenuEntry;
+		_menuContainer.AddChild(settingsEntry);
+		settingsEntry.Setup(UIBaseMenuEntry.MainMenuType.Settings);
+		_menuEntries.Add(settingsEntry);
+
+		UIBaseMenuEntry saveEntry = menuEntryScene.Instantiate() as UIBaseMenuEntry;
+		_menuContainer.AddChild(saveEntry);
+		saveEntry.Setup(UIBaseMenuEntry.MainMenuType.Save);
+		_menuEntries.Add(saveEntry);
+
+		// Party Setup
 		foreach(ActivePartyMember partyMember in gameState.GetActivePartyMembers())
 		{
 			UIPartyMenuEntry partyMenuEntry = partyMenuEntryScene.Instantiate() as UIPartyMenuEntry;
-			AddChild(partyMenuEntry);
+			_partyContainer.AddChild(partyMenuEntry);
 
 			partyMenuEntry.Setup(partyMember);
 		}
 
 		_goldDisplay.Setup(gameState.GetGoldAmount());
+
+		index = 0;
 	}
 }
---
+++ src/overworld/overworldMenu/baseMenuEntry/UIBaseMenuEntry.cs
@@ -0,0 +1,82 @@
+using Godot;
+using System;
+
+public partial class UIBaseMenuEntry : Control
+{
+	[Export] Color choiceColor = new Color("#fbff00");
+	[Export] Color defaultColor = new Color("#ffffff");
+
+	Label _textLabel;
+	public enum MainMenuType { Skills, Items, Equipment, Party, Settings, Save };
+	MainMenuType _mainMenuType;
+
+	bool highlighted = false;
+	public bool Highlighted
+	{
+		get { return highlighted; }
+		set
+		{
+			highlighted = value;
+			if (highlighted)
+			{
+				_textLabel.Modulate = choiceColor;
+			} else {
+				_textLabel.Modulate = defaultColor;
+			}
+		}
+	}
+
+	public override void _Ready()
+	{
+		_textLabel = GetNode<Label>("HBoxContainer/Label");
+	}
+
+	public void Setup(MainMenuType menuType)
+	{
+		_mainMenuType = menuType;
+
+		string text = "";
+		switch (_mainMenuType)
+		{
+			case MainMenuType.Skills:
+			{
+				text = "Skills";
+				break;
+			}
+
+			case MainMenuType.Items:
+			{
+				text = "Items";
+				break;
+			}
+
+			case MainMenuType.Equipment:
+			{
+				text = "Equipment";
+				break;
+			}
+
+			case MainMenuType.Party:
+			{
+				text = "Party";
+				break;
+			}
+
+			case MainMenuType.Settings:
+			{
+				text = "Settings";
+				break;
+			}
+
+			case MainMenuType.Save:
+			{
+				text = "Save";
+				break;
+			}
+		}
+
+		_textLabel.Text = text;
+	}
+
+	public MainMenuType GetItemOptionMenuType() { return _mainMenuType; }
+}
